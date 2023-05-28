3a5b196cf6fa02e645918fab7fecadc849a93eb2
gkiri <hkkiri@gmail.com>
2023-05-28 17:15:16 +0200

    Update App.tsx final mainnet changes

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,11 @@ import { useMemo, useState } from 'react';
 import { Toast } from './components/Toast';
 import { parseIneligibility } from './utils/parseIneligibility';
 import { useToast } from './hooks/useToast';
-//import presale1CSV from "./presale1.csv";
 import { useEffect } from 'react';
-import Timer from "./components/Timer";
 import BADBOYZ from "./images/BADBOYZ.png";
 
 const urlParams = new URL(window.location.toString()).searchParams;
-const contractAddress = urlParams.get('contractAddress') || '0xb703af6065bAD031423a5029Db2Be53db154c563';
+const contractAddress = urlParams.get('contractAddress') || '0x8EDEeC3DaB0e3249909B3E70C0dE84Ca268987ef';
 
 export default function Home() {
   const contractQuery = useContract(contractAddress);
@@ -233,8 +231,8 @@ export default function Home() {
       if (pricePerToken.eq(0)) {
         return 'Mint (Free)';
       }
-      //return `Mint (${priceToMint})`;//gkiri orig
-      return `Mint`;
+      return `Mint (${priceToMint})`;//gkiri orig
+      //return `Mint`;
     }
     if (claimIneligibilityReasons.data?.length) {
       return parseIneligibility(claimIneligibilityReasons.data, quantity);
@@ -285,17 +283,130 @@ export default function Home() {
 
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center py-4 mb-10">{/* Add margin-bottom (mb-10) here */}
-        <div className="flex flex-col items-center mt-2">
-          <img src={BADBOYZ} alt="BadBoyz" className="responsive-image" />
+
+        <div className="flex flex-col items-center mt-6">
+            <img src={BADBOYZ} alt="BadBoyz" className="responsive-image" style={{ marginTop: '6rem' }}/>
         </div>
 
-        <div className="countdown-timer">
-          {/*<h1>Countdown</h1>*/}
-          <Timer mintDate={mintDate} />
-        </div> 
-      </div>
-
+        <div className="flex items-center justify-center w-full h-full col-span-1 lg:col-span-7 " >
+          <div className="flex flex-col gap-4 p-6 lg:border rounded-xl lg:border-gray-800 mint-box2 mt-[-16.5rem] " >
+            <div className="flex w-full gap-4 rounded-xl ">
+              {dropNotReady ? (
+                <span className="text-red-500">
+                  This drop is not ready to be minted yet. (No claim condition
+                  set)
+                </span>
+              ) : dropStartingSoon ? (
+                <span className="text-gray-500">
+                  Drop is starting soon. Please check back later.
+                </span>
+              ) : (
+                <div className="flex flex-col w-full gap-2 ">
+                  <div className="flex flex-col w-full gap-2 lg:gap-2 lg:flex-row lg:items-center ">
+                    <div>
+                      <div className="flex h-12 px-2 border border-gray-800 rounded-lg" >
+                        <button
+                          onClick={() => {
+                            const value = quantity - 1;
+                            if (value > maxClaimable) {
+                              setQuantity(maxClaimable);
+                            } else if (value < 1) {
+                              setQuantity(1);
+                            } else {
+                              setQuantity(value);
+                            }
+                          }}
+                          className="flex items-center justify-center h-full px-2 text-2xl text-center text-white rounded-l-md"
+                          disabled={isSoldOut}
+                        >
+                          -
+                        </button>
+                        <p className="flex items-center justify-center w-full h-full font-mono text-center text-white lg:w-64">
+                          {!isLoading && isSoldOut ? 'Sold Out' : quantity}
+                        </p>
+                        <button
+                          onClick={() => {
+                            const value = quantity + 1;
+                            if (value > maxClaimable) {
+                              setQuantity(maxClaimable);
+                            } else if (value < 1) {
+                              setQuantity(1);
+                            } else {
+                              setQuantity(value);
+                            }
+                          }}
+                          className="flex items-center justify-center h-full px-2 text-2xl text-center text-white rounded-r-md"
+                          disabled={isSoldOut}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+                    {address ? (
+                      <Web3Button
+                        className="mint-button" // Add this line
+                        contractAddress={
+                          contractQuery.contract?.getAddress() || ''
+                        }
+                        action={(cntr) => cntr.erc721.claim(quantity)}
+                        isDisabled={!canClaim || buttonLoading}
+                        onError={(err) => {
+                          console.error(err);
+                          console.log({ err });
+                          showToast({
+                            title: 'Failed to mint drop',
+                            description: (err as any).reason || '',
+                            status: 'error',
+                            duration: 9000,
+                            isClosable: true,
+                          });
+                        }}
+                        onSuccess={() => {
+                          showToast({
+                            title: 'Successfully minted',
+                            description:
+                              'The NFT has been transferred to your wallet',
+                            status: 'success',
+                            duration: 5000,
+                            isClosable: true,
+                          });
+                        }}
+                      >
+                        {buttonLoading ? (
+                          <div role="status">
+                            <svg
+                              aria-hidden="true"
+                              className="w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                              viewBox="0 0 100 101"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"
+                              />
+                              <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill"
+                              />
+                            </svg>
+                            <span className="sr-only">Loading...</span>
+                          </div>
+                        ) : (
+                          buttonText
+                        )}
+                      </Web3Button>
+                    ) : (
+                      <ConnectWallet />
+                    )}
+                    
+                  </div>
+                  <Toast toast={toast} onClose={hideToast} />
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
